6d2a4d1771104c28475b434be902f93214552b2e
Charles-Antoine Pouliot <charles-antoine2001@outlook.com>
2021-10-15 12:42:43 -0400

    première implémentation des sons

--- Assets/Scripts/managePlayerLives.cs
+++ Assets/Scripts/managePlayerLives.cs
@@ -7,28 +7,52 @@ public class ManagePlayerLives : MonoBehaviour
     [SerializeField] private int nbOfLives = 5;
     [SerializeField] private CharacterController characterController;
     [SerializeField] GameManager gameManager;
+    private AudioSource audioSource1; //Son de blessure
+    private AudioSource audioSource2; //Son de mort
+    private bool isInvincible = false;
+    private int invincibleTime = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource1 = GetComponents<AudioSource>()[0];
+        audioSource2 = GetComponents<AudioSource>()[1];
+        gameManager.modifyLifeCounter(nbOfLives);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isInvincible && invincibleTime <= 0)
+        {
+            isInvincible = false;
+            invincibleTime = 30;
+            Debug.Log("Invincible OFF");
+        }
+        else
+        {
+            invincibleTime -= 1;
+        }
     }
 
     private void OnControllerColliderHit(ControllerColliderHit collision)
     {
-        if(collision.gameObject.tag == "alien")
+        if(collision.gameObject.tag == "alien" && isInvincible == false)
         {
+            collision.gameObject.GetComponent<AudioSource>().Play();
             Destroy(collision.gameObject);
             if (characterController.isGrounded || gameObject.transform.position.y <= collision.gameObject.transform.position.y)
             {
                 nbOfLives = nbOfLives - 1;
-                Debug.Log(nbOfLives);
+                isInvincible = true;
+                Debug.Log("Invincible ON");
+                audioSource1.Play();
                 gameManager.modifyLifeCounter(nbOfLives);
+            }
+            if(nbOfLives <= 0)
+            {
+                audioSource2.Play();
+                Destroy(gameObject);
             }
         }
     }
